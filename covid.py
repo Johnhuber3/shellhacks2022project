c9a61ff423c6b3256012cade7a0533aef258645d
khoivu166 <khoivu166@gmail.com>
2022-09-10 04:38:34 -0400

    Khoi and John - Filtered Data (Date, Country, Confirmed) to only G20 (Excluding EU). Dump data into JSON for easier processing.

---
+++ covid.py
@@ -0,0 +1,21 @@
+import pandas as pd;
+import json;
+
+# read csv of fullGroup
+fullGroup = pd.read_csv('./data/full_grouped.csv');
+
+# condense data to Date, Country, Confirmed 
+condensed = pd.DataFrame(fullGroup, columns=['Date', 'Country/Region', 'Confirmed'])
+
+# filtered to only G19 countries
+G19 = ['Argentina', 'Australia', 'Brazil', 'Canada', 'China', 'France', 'Germany', 'India', 'Indonesia', 'Italy', 'Japan', 'South Korea', 'Mexico', 'Russia', 'Saudi Arabia', 'South Africa', 'Turkey', 'United Kingdom', 'US']
+df = pd.DataFrame(condensed.loc[condensed['Country/Region'].isin(G19)]);
+# make dataframe a dictionary
+g19Dict = df.to_dict();
+
+print(df.head())
+
+# save to JSON File
+with open('data.json', 'w') as f: 
+    json.dump(g19Dict, f, ensure_ascii=False, indent=4);
+
